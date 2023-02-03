5cb293eee9d116e22b92abba99a8b86dbaa703d9
Jonathan Mani <social.chapters@simplelogin.com>
2023-02-03 20:54:08 +0000

    added Filterable Product Table

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function FilterableProductTable(){
-    return (
-        <>
-            <SearchBar>
-            <ProductTable>
-        </>
-    )
+export default function FilterableProductTable() {
+  return (
+    <>
+      <SearchBar />
+      <ProductTable />
+    </>
+  );
 }
